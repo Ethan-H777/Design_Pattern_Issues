9ddb47d89127ac60b26c0829e7e45b61996533a8
Ethan He <yihe7176@uni.sydney.edu.au>
2023-05-30 20:51:54 +1000

    change to flyweight class

--- src/main/java/au/edu/sydney/brawndo/erp/spfea/products/ProductFlyweightFactory.java
+++ src/main/java/au/edu/sydney/brawndo/erp/spfea/products/ProductFlyweightFactory.java
@@ -1,21 +1,28 @@
 package au.edu.sydney.brawndo.erp.spfea.products;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ProductFlyweightFactory {
-    private Map<double[], ProductData> flyweights;
+    private Map<String, ProductData> flyweights;
 
     public ProductFlyweightFactory() {
         this.flyweights = new HashMap<>();
     }
 
-    public ProductData getProductData(double[] data) {
-        if (!flyweights.containsKey(data)) {
+    public ProductData getProductData(double[] manufacturingData,
+                                      double[] recipeData,
+                                      double[] marketingData,
+                                      double[] safetyData,
+                                      double[] licensingData) {
+        String key = Arrays.toString(manufacturingData) + Arrays.toString(recipeData) + Arrays.toString(marketingData) + Arrays.toString(safetyData) + Arrays.toString(licensingData);
+
+        if (!flyweights.containsKey(key)) {
             //create new data flyweight
-            flyweights.put(data, new ProductData(data));
+            flyweights.put(key, new ProductData(manufacturingData, recipeData, marketingData, safetyData, licensingData));
         }
 
-        return flyweights.get(data);
+        return flyweights.get(key);
     }
 }
